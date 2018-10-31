7883812977b9e934a163c78c7bca38567ace0886
Ved Smithiprechawongse <vedsmith92@gmail.com>
2018-10-31 19:40:55 +0700

    Build `map subscribes to props`

--- dist/unstated-connect.umd.js
+++ dist/unstated-connect.umd.js
@@ -8,20 +8,22 @@
 
   var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-  var connect = (function (_containers) {
-    return function (Component) {
-      return function (props) {
-        return React.createElement(
-          unstated.Subscribe,
-          { to: _containers },
-          function () {
-            for (var _len = arguments.length, containers = Array(_len), _key = 0; _key < _len; _key++) {
-              containers[_key] = arguments[_key];
-            }
+  var connect = (function (containers) {
+    return function (mapper) {
+      return function (Component) {
+        return function (props) {
+          return React.createElement(
+            unstated.Subscribe,
+            { to: containers },
+            function () {
+              for (var _len = arguments.length, containers = Array(_len), _key = 0; _key < _len; _key++) {
+                containers[_key] = arguments[_key];
+              }
 
-            return React.createElement(Component, _extends({}, props, { containers: containers }));
-          }
-        );
+              return React.createElement(Component, _extends({}, props, mapper(containers)));
+            }
+          );
+        };
       };
     };
   });
